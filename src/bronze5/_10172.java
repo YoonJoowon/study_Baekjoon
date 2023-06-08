c61040708da54c9e9504b506e52f557a47e3ad25
yoonjoowon <wndnjs6224@naver.com>
2023-06-08 15:28:31 +0900

    study : 코딩 테스트 공부

--- src/bronze5/_10172.java
+++ src/bronze5/_10172.java
@@ -3,11 +3,10 @@ package bronze5;
 public class _10172 {
 
     public static void main(String[] args) {
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
 
 
     }
