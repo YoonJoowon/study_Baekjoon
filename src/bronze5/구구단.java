abcf0f7e607a63311f3248bc1309d4eba10dd6f4
yoonjoowon <wndnjs6224@naver.com>
2023-06-09 18:14:59 +0900

    study

---
+++ src/bronze5/구구단.java
@@ -0,0 +1,19 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class 구구단 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        in.close();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(a + " * " + i + " = " + (a * i));
+        }
+    }
+
+}
